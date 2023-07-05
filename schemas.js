b0250285b314d3292b3cc62c042fdc481d39cfc9
luigipr <luigiprofeti@gmail.com>
2023-07-05 17:45:43 -0300

    versão 4 - versão pra tentar deploy

--- schemas.js
+++ schemas.js
@@ -2,18 +2,18 @@ import Joi from "joi";
 
 export const userSchema = Joi.object({
     name: Joi.string().required(),
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(3),
-    confirmPassword:  Joi.string().required().min(3)
+    email: Joi.string().email().required(),
+    password: Joi.string().min(3).required(),
+    confirmPassword:  Joi.string().min(3).required()
 })
 
 export const loginSchema = Joi.object({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(3)
+    email: Joi.string().email().required(),
+    password: Joi.string().min(3).required()
 })
 
 export const transactionSchema = Joi.object({
-    value: Joi.required().number().precision(2),
-    description: Joi.required().string(),
-    date: Joi.required().date().format('DD-MM')
+    value: Joi.number().precision(2).required(),
+    description: Joi.string().required(),
+    date: Joi.date().format('DD-MM').required()
 })
